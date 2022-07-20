d4dff408e45c7f7eff24e1801297354d01b120cf
jhkl0514 <jhkl0514@naver.com>
2022-07-20 18:19:17 +0900

    0720

--- 07company_test/js/script.js
+++ 07company_test/js/script.js
@@ -1,1 +1,23 @@
 console.log("test");
+
+// 메뉴
+$(".navbar>ul>li").hover(function(){
+    $(this).addClass("on");
+
+},function(){
+    $(this).removeClass("on");
+
+})
+
+// 토글
+$(".toggle").click(function(){
+    $(".navbarClone").css("left",0);
+    $(".overlay").show();
+})
+$(".overlay").click(function(){
+    $(".navbarClone").css("left","-100%");
+    $(".overlay").hide();
+})
+
+// navbar를 자져와서 복제하고 appendTo를 한다 (mMenu)
+$(".navbar>ul").clone().appendTo(".mMenu")
