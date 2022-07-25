f7f4fa5ce9ec8466a965429951c655acd55184dd
jhkl0514 <105696294+jhkl0514@users.noreply.github.com>
2022-07-26 00:08:33 +0900

    0725

--- 07company/js/script.js
+++ 07company/js/script.js
@@ -11,14 +11,25 @@ console.log("test");
 // 토글
 $(".toggle").click(function(){
     $(".navbarClone").css("left",0);
+    $(".toggle").addClass("active")
     $(".overlay").show();
 })
 $(".overlay").click(function(){
     $(".navbarClone").css("left","-100%");
+    $(".toggle").removeClass("active")
     $(".overlay").hide();
+
 })
+
 
 // navbar를 자져와서 복제하고 appendTo를 한다 (mMenu)
 $(".navbar>ul").clone().appendTo(".mMenu")
 
 
+$(".navbarClone .mMenu>ul>li").mouseover(function(){
+    $(this).find(".submenu").stop().slideDown(200);
+  })
+
+  $(".navbarClone .mMenu>ul>li").mouseout(function(){
+    $(this).find(".submenu").stop().slideUp(200);
+  })
