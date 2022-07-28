a62bde7ab7dc8af942eec26fd6be9415c9440e0b
jhkl0514 <105696294+jhkl0514@users.noreply.github.com>
2022-07-29 04:15:48 +0900

    script

--- 07company/js/script.js
+++ 07company/js/script.js
@@ -11,12 +11,12 @@ console.log("test");
 // 토글
 $(".toggle").click(function(){
     $(".navbarClone").css("left",0);
-    $(".toggle").addClass("active"); 
+    $(".toggle").addClass("active")
     $(".overlay").show();
 })
 $(".overlay").click(function(){
     $(".navbarClone").css("left","-100%");
-    $(".toggle").removeClass("active");
+    $(".toggle").removeClass("active")
     $(".overlay").hide();
 
 })
@@ -34,20 +34,45 @@ $(".navbarClone .mMenu>ul>li").mouseover(function(){
     $(this).find(".submenu").stop().slideUp(200);
   })
 
-//  메인 Initialize Swiper
-        var swiper = new Swiper(".mySwiper", {
-          slidesPerView: 1,
-          loop: true,
-          pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-          },
-          navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-          },
-        });
- 
+  //  메인 Initialize Swiper
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
 
 // 메인 스와이퍼 를 자져와서 복제하고 appendTo를 한다 ()
 $(".visualWrap>div").clone().appendTo(".mMswiper")
+
+//news01 Swiper
+var swiper = new Swiper(".mySwiper_02", {   
+    loop: true,
+    autoplay: {
+        delay: 2000,
+      }    
+});
+var swiper = new Swiper(".mySwiper_03", {   
+    loop: true,
+    autoplay: {
+        delay: 3000,
+      }
+    
+});
+
+var swiper = new Swiper(".mySwiper_04", {   
+    loop: true,
+    autoplay: {
+        delay: 4000,
+      }
+    
+});
+
+// bxSlider
