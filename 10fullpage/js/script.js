ecced2485fd48efbff2e5c6d90b5c6f921ec370d
jhkl0514 <jhkl0514@naver.com>
2022-09-23 18:20:54 +0900

    0923_01

--- 10fullpage/js/script.js
+++ 10fullpage/js/script.js
@@ -1,8 +1,32 @@
-$('#fullpage').fullpage({
+$("#fullpage").fullpage({
     menu:".navTop",
-    anchors:["m1st","m2st","m3st","m3st"],
+    anchors:["m1st","m2st","m3st","m4st"],
     navigation:true,
+    navigationColor:"#fff",
     navigationPosition:"fp-right",
-    navigationTooltips:["first","second","third","fouth"],
-    responsiveWidth:900
+    navigationTooltips:['1','2','3','4'],
+    responsiveWidth:900,
+    afterLoad:function(index){
+        console.log(anchorslink, index);
+        if(index == 1){
+            $(".page1 .wrap").addClass("move")
+        }else{
+            $(".page1 .wrap").removeClass("move")
+        }
+        if(index == 2){
+            $(".page2 .wrap").addClass("move")
+        }else{
+            $(".page2 .wrap").removeClass("move")
+        }
+        if(index == 3){
+            $(".page3 .wrap").addClass("move")
+        }else{
+            $(".page3 .wrap").removeClass("move")
+        }
+        if(index == 4){
+            $('.page4').addClass('move')
+        }else{
+            $('.page4').removeClass('move')
+        }              
+    }
 })
